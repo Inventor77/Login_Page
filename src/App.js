b1610e290548191a903d57c5eef6c22715d3d470
Shreedutt <shreedutt77@gmail.com>
2023-01-15 01:09:22 +0530

    Custom Theme is been added to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.scss';
 import LoginPage from './pages/LoginPage';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#4061FC",
+		},
+	},
+});
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
-  );
+		<ThemeProvider theme={theme}>
+			<div className='App'>
+				<LoginPage />
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
