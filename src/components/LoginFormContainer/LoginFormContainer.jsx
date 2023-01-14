3bcbaef75766119a6ac5bd087a0d841eea9d38be
Shreedutt <shreedutt77@gmail.com>
2023-01-15 00:16:06 +0530

    password and email input field is added the the LoginFormContainer

--- src/components/LoginFormContainer/LoginFormContainer.jsx
+++ src/components/LoginFormContainer/LoginFormContainer.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
-import './LoginFormContainer.scss'
+import React, { useState } from "react";
+import "./LoginFormContainer.scss";
+import {
+	FormControl,
+	InputLabel,
+	InputAdornment,
+	IconButton,
+	OutlinedInput,
+	TextField,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 function LoginFormContainer() {
-  return (
-      <div className='login_form-container'>
-          
-    </div>
-  )
+	const [showPassword, setShowPassword] = useState(false);
+
+	const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+	return (
+		<div className='login_form-container'>
+			<div style={{marginBottom: "28px"}}>
+        <InputLabel
+					htmlFor='textarea'
+					sx={{
+						marginBottom: "12px",
+						fontSize: "14px",
+						color: "rgba(24,24,24,.8)",
+					}}>
+					Work Email
+				</InputLabel>
+				<FormControl sx={{ width: "100%" }} variant='outlined'>
+          <TextField
+            type="email"
+            id='textarea' placeholder='janedoe@abc.com' size='small' />
+				</FormControl>
+			</div>
+			<div>
+				<InputLabel
+					htmlFor='password'
+					sx={{
+						marginBottom: "12px",
+						fontSize: "14px",
+						color: "rgba(24,24,24,.8)",
+					}}>
+					Password
+				</InputLabel>
+				<FormControl sx={{ width: "100%" }} variant='outlined' size='small'>
+					<OutlinedInput
+						id='password'
+						type={showPassword ? "text" : "password"}
+						endAdornment={
+							<InputAdornment position='end'>
+								<IconButton onClick={handleClickShowPassword} edge='end'>
+									{showPassword ? <VisibilityOff /> : <Visibility />}
+								</IconButton>
+							</InputAdornment>
+						}
+					/>
+				</FormControl>
+			</div>
+		</div>
+	);
 }
 
-export default LoginFormContainer
+export default LoginFormContainer;
