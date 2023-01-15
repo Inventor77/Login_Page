b7741c529e2919e367c32dd9eb07843687c0d970
Shreedutt <shreedutt77@gmail.com>
2023-01-15 11:44:48 +0530

    improved useFetch to be more dynamic

--- src/utils/hooks/useFetch.jsx
+++ src/utils/hooks/useFetch.jsx
@@ -1,16 +1,27 @@
 import { useState, useEffect } from "react";
 
-function useFetch(url) {
+export function useFetch(url, method = "GET", body) {
 	const [data, setData] = useState(null);
 
+	let fetchArg;
+	if (method === "GET") {
+		fetchArg = [url];
+	} else if (method === "POST") {
+		fetchArg = [
+			url,
+			{
+				method,
+				body,
+			},
+		];
+	}
+
 	useEffect(() => {
-		fetch(url)
+		fetch(...fetchArg)
 			.then((res) => res.json())
 			.then((data) => setData(data))
 			.catch((err) => console.log(err));
-	}, [url]);
+	}, [url, method, body]);
 
 	return [data];
 }
-
-export default useFetch;
